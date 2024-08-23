16c740aac0051156e15a8fdc3a440b91e03e5b00
Furqan <elliotgould5721@gmail.com>
2024-08-23 11:50:17 +0100

    Props and Prop drilling

--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,10 @@
-const Footer = () => {
+const Footer = ({length}) => {
 
     const today = new Date();
 
     return (
         <footer>
-            <p>Copyright &copy; {today.getFullYear()}</p>
+            <p>{length} List {length === 1 ? 'Item' : 'Items'}</p>
         </footer>
     )
 }
--- src/header.js
+++ src/header.js
@@ -1,9 +1,13 @@
-const Header = () => {
+const Header = (props) => {
   return (
     <header>
-      <h1>Groceries Lists</h1>
+      <h1>{props.title}</h1>
     </header>
   );
 };
+
+Header.defaultProps = {
+  title: "Default Title"
+}
 
 export default Header;
