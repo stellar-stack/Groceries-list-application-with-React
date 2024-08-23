ae07ce0a2747e46f99e4f83320f329c5b9d5c7c3
Furqan <elliotgould5721@gmail.com>
2024-08-23 10:35:35 +0100

    usnig react hooks

--- src/constent.js
+++ src/constent.js
@@ -1,27 +1,32 @@
+import { useState } from "react";
+
 const Content = () => {
+  const [name, setName] = useState("Dave");
+  const [count, setCount] = useState(0);
+
+
   const handleNameChange = () => {
     const name = ["bob", "kevin", "Dave"];
     const int = Math.floor(Math.random() * 3);
-    return name[int];
+    setName(name[int]);
   };
 
   const handleClick = () => {
-    console.log("You Clicked It");
+    setCount(count + 1);
+    console.log(count);
   };
-  const handleClick2 = (name) => {
-    console.log(`${name} was Clicked`);
+  const handleClick2 = () => {
+    console.log(count);
   };
-  const handleClick3 = (e) => {
-    console.log(e.target.innerText);
-  };
+ 
 
   return (
     <main>
-      <p onDoubleClick={handleClick}>Hello {handleNameChange()}</p>
+      <p onDoubleClick={handleClick}>Hello {name}</p>
 
+      <button onClick={handleNameChange}>Change name</button>
       <button onClick={handleClick}>Click It</button>
-      <button onClick={() => handleClick2("Dave")}>Click It</button>
-      <button onClick={(e) => handleClick3(e)}>Click It</button>
+      <button onClick={handleClick2}>Click It</button>
     </main>
   );
 };
